92097d482de3bdfe4565f8edca6f437a3f1be5e2
Alexander <jdaanov@gmail.com>
2023-11-10 16:27:33 -0800

    Create rdbrck_test_puppeteer.js
    3 Tests created based on definition, \
    Navigating to “https://rdbrck.com” will redirect to “https://www.rdbrck.com”.
    Once on “https://www.rdbrck.com”, validate that clicking the “Careers” button in the top header will navigate to “https://www.rdbrck.com/careers”
    Validate that the “Lead QA Automation Developer” job card exists on the careers page.
    After validating the above behaviour, the test suite should complete and report its findings and the instance of Chromium should close.

---
+++ rdbrck_test_puppeteer.js
@@ -0,0 +1,69 @@
+const puppeteer = require('puppeteer');
+
+async function runTests() {
+  const browser = await puppeteer.launch({
+    headless: false, // Run Chromium with a GUI (visible browser window), as required
+    defaultViewport: null, // Adjust viewport settings
+  });
+
+  try {
+    const page = await browser.newPage();
+
+    // First test: Navigating to “https://rdbrck.com” will redirect to “https://www.rdbrck.com”.
+    await testRedirectTo(page);
+
+    // Second test: Once on “https://www.rdbrck.com”, validate that clicking the “Careers” button in the top header will navigate to “https://www.rdbrck.com/careers”
+    await testNavToCareers(page);
+
+    // Third test: Validate that the “Lead QA Automation Developer” job card exists on the careers page.
+    await testJobCardExist(page);
+
+  } finally {
+    await browser.close();
+  }
+}
+// First test functionality
+async function testRedirectTo(page) {
+  await page.goto('https://rdbrck.com');
+  // Wait for any potential redirects
+  await page.waitForTimeout(2000);
+  const currentUrlAfterRedirect = page.url();
+  console.log(`Test 1: Current URL after redirect: ${currentUrlAfterRedirect}`);
+
+  if (currentUrlAfterRedirect === 'https://www.rdbrck.com') {
+    console.log('Test 1 passed: Redirect to "https://www.rdbrck.com" successful.');
+  } else {
+    console.error('Test 1 failed: Redirect did not happen as expected.');
+  }
+}
+// Second test functionality
+async function testNavToCareers(page) {
+  await page.goto('https://www.rdbrck.com');
+  await page.click('a[href="/careers"]');
+  // Wait for any potential navigation
+  await page.waitForTimeout(2000);
+  const urlAfterCareersClick = page.url();
+  console.log(`Test 2: Current URL after clicking "Careers": ${urlAfterCareersClick}`);
+
+  if (urlAfterCareersClick === 'https://www.rdbrck.com/careers') {
+    console.log('Test 2 passed: Navigation to "https://www.rdbrck.com/careers" successful.');
+  } else {
+    console.error('Test 2 failed: Navigation to "https://www.rdbrck.com/careers" did not happen as expected.');
+  }
+}
+// Third test functionality
+async function testJobCardExist(page) {
+  await page.goto('https://www.rdbrck.com/careers');
+  const jobCardExist = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('.job-card')).some(card => card.innerText.includes('Lead QA Automation Developer'));
+  });
+  console.log(`Test 3: Lead QA Automation Developer job card exists: ${jobCardExists}`);
+
+  if (jobCardExist) {
+    console.log('Test 3 passed: "Lead QA Automation Developer" job card exists on the careers page.');
+  } else {
+    console.error('Test 3 failed: "Lead QA Automation Developer" job card not found.');
+  }
+}
+//Tests executor
+runTests();
