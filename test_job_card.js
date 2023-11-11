528b51011ff33fe82dc513ecc990be40fa6dcfce
Alexander <jdaanov@gmail.com>
2023-11-11 09:42:27 -0800

    Create test_job_card.js
    Added job card validation test

---
+++ test_job_card.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+const CommonPage = require('./pageObjects');
+
+class CareersPage extends CommonPage {
+  async jobCardExists() {
+    return await this.page.$eval('.job-card', (card) => card !== null); // need to adjust selector as per the actual job card class
+  }
+}
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const commonPage = new CommonPage(page);
+  const careersPage = new CareersPage(page);
+
+  try {
+    // Test 3: Validate that the "Lead QA Automation Developer" job card exists on the careers page.
+    await commonPage.navigateTo('https://www.rdbrck.com/careers');
+    const jobCardExists = await careersPage.jobCardExists();
+    if (jobCardExists) {
+      console.log('Test 3 Passed: "Lead QA Automation Developer" job card exists');
+    } else {
+      console.error('Test 3 Failed: "Lead QA Automation Developer" job card not found');
+    }
+  } catch (error) {
+    console.error('Error during test:', error);
+  } finally {
+    await browser.close();
+  }
+})();
