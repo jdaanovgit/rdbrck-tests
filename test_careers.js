4c50ef52da2702a40c739a1d342859bf9df9f7ff
Alexander <jdaanov@gmail.com>
2023-11-11 09:38:18 -0800

    Create test_careers.js
    Created new test for careers check

---
+++ test_careers.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const CommonPage = require('./pageObjects');
+
+class CareersPage extends CommonPage {
+  async navigateToCareers() {
+    await this.page.click('button#careers'); // need to adjust selector as per the actual button ID or class
+  }
+}
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const commonPage = new CommonPage(page);
+  const careersPage = new CareersPage(page);
+
+  try {
+    // Test 2: Validate that clicking the "Careers" button navigates to "https://www.rdbrck.com/careers".
+    await commonPage.navigateTo('https://www.rdbrck.com');
+    await careersPage.navigateToCareers();
+    const currentUrl = page.url();
+    if (currentUrl === 'https://www.rdbrck.com/careers') {
+      console.log('Test 2 Passed: Navigation to Careers successful');
+    } else {
+      console.error(`Test 2 Failed: Expected navigation to https://www.rdbrck.com/careers, but got ${currentUrl}`);
+    }
+  } catch (error) {
+    console.error('Error during test:', error);
+  } finally {
+    await browser.close();
+  }
+})();
