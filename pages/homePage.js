8fe3565de7d54748d1ed7087b963118c610429a3
Alexander <jdaanov@gmail.com>
2023-11-14 09:32:10 -0800

    Update homePage.js

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,1 +1,11 @@
+const BasePage = require('./basePage');
+const { HOMEPAGE_LOCATORS } = require('../locators/homePageLocators');
+
+class HomePage extends BasePage {
+  
+  // homePage-specific methods wil be added here
+  
+}
+
+module.exports = HomePage;
 
