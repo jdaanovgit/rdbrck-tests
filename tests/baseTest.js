3a0843ca3d16d3456dae4149088d1364d26c62c5
Alexander <jdaanov@gmail.com>
2023-11-14 10:35:49 -0800

    Update baseTest.js

--- tests/baseTest.js
+++ tests/baseTest.js
@@ -1,1 +1,15 @@
 // Browser initiation, read site url from ENV variables
+const env = require('./env');
+
+class BaseTest {
+  constructor() {
+    // Initializing test setup
+  }
+
+  async run() {
+    console.log(`Running test on site: ${env.SITE_URL}`);
+    // 
+  }
+}
+
+module.exports = BaseTest;
