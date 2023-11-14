5ee86824cc394d03365af10bb52f6001ccc3d60b
Alexander <jdaanov@gmail.com>
2023-11-14 09:30:56 -0800

    Update basePage.js

--- pages/basePage.js
+++ pages/basePage.js
@@ -1,1 +1,13 @@
+class BasePage {
+  constructor(page) {
+    this.page = page;
+  }
 
+  async navigateTo(url) {
+    await this.page.goto(url);
+  }
+
+  // Common page methods will be added here
+}
+
+module.exports = BasePage;
