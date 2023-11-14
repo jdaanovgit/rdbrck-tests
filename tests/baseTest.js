e981d7932f10c13b7ffa51a2583947d947cadce1
Alexander <jdaanov@gmail.com>
2023-11-14 13:29:45 -0800

    Update baseTest.js

--- tests/baseTest.js
+++ tests/baseTest.js
@@ -1,25 +1,19 @@
 // Browser initiation, read site url from ENV variables
 
 const puppeteer = require('puppeteer');
-const env = require('../env'); 
 
-class BaseTest {
-  constructor() {
-    this.browser = null;
-    this.page = null;
-  }
-
-  async setup() {
-    // Initialize test setup, browser, etc.
-    this.browser = await puppeteer.launch({ headless: false });
-    this.page = await this.browser.newPage();
-    await this.page.setViewport({ width: 1920, height: 1080 });
-  }
-
-  async close() {
-    // Cleanup after the test
-    await this.browser.close();
-  }
+async function setup() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 });
+  return { browser, page };
 }
 
-module.exports = BaseTest;
+async function close({ browser }) {
+  await browser.close();
+}
+
+module.exports = {
+  setup,
+  close,
+};
