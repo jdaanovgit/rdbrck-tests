009174a580f518e37db31d844c2bc1402e4d62cc
Alexander <jdaanov@gmail.com>
2023-11-13 21:45:53 -0800

    Create test_job_card.js

---
+++ tests/test_job_card.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+const assert = require('assert');
+const getOpenPositionTitles = require('./getOpenPositionTitles');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 }); // set full-size window
+
+  try {
+    // Navigate directly using the page object
+    await page.goto('https://www.rdbrck.com/careers');
+
+    // Use the getOpenPositionTitles method to get a list of open positions
+    const openPositionTitles = await getOpenPositionTitles(page);
+
+    // Validation: Check if "Lead QA Automation Developer" is in the list of open positions
+    const positionToFind = 'Lead QA Automation Developer';
+    assert(openPositionTitles.includes(positionToFind), `${positionToFind} - does not appear in the list`);
+
+    console.log(`${positionToFind} - found in the list`);
+
+    // If the position is found, mark the test as passed
+    console.log('Test Passed!');
+  } catch (error) {
+    console.error('Error:', error.message);
+  } finally {
+    await browser.close();
+  }
+})();
