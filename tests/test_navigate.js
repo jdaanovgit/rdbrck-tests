5a2f1804070ab1ff3e3f51a9f90a28a6aea1c8ef
Alexander <jdaanov@gmail.com>
2023-11-13 21:46:28 -0800

    Create test_navigate.js

---
+++ tests/test_navigate.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+const CommonPage = require('./pageObjects');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 }); // Set to full HD resolution
+  const commonPage = new CommonPage(page);
+
+  try {
+    // Test 1: Navigating to "https://rdbrck.com" will redirect to "https://www.rdbrck.com".
+    await commonPage.navigateTo('https://rdbrck.com');
+    const currentUrl = page.url();
+    if (currentUrl === 'https://www.rdbrck.com/') { //can add another assert by home page text, to be sure that url doesn't includes some query params.
+      console.log('Test 1 Passed: Redirect successful');
+    } else {
+      console.error(`Test 1 Failed: Expected redirection to https://www.rdbrck.com, but got ${currentUrl}`);
+    }
+  } catch (error) {
+    console.error('Error during test:', error);
+  } finally {
+    await browser.close();
+  }
+})
+();
