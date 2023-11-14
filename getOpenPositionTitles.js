a66bdf8dbbc701e738effdca3ac9e3df2470c806
Alexander <jdaanov@gmail.com>
2023-11-14 15:14:32 -0800

    Update getOpenPositionTitles.js

--- getOpenPositionTitles.js
+++ getOpenPositionTitles.js
@@ -1,15 +1,14 @@
+
 const puppeteer = require('puppeteer');
-const careersLocator = require('./locators/careersPageLocators');
+const careersLocator = require('../locators/careersPageLocators'); 
 
 async function getOpenPositionTitles(page) {
-  const { OPEN_POSITION_TITLES } = careersLocator;
-
   // Wait for the open positions to load
-  await page.waitForSelector(OPEN_POSITION_TITLES);
+  await page.waitForSelector(careersLocator.OPEN_POSITION_TITLES);
 
   // Extract text content from open position elements
   const openPositionTitles = await page.evaluate(() => {
-    const positionElements = document.querySelectorAll(OPEN_POSITION_TITLES);
+    const positionElements = document.querySelectorAll(careersLocator.OPEN_POSITION_TITLES);
     return Array.from(positionElements, (element) => element.textContent.trim());
   });
 
