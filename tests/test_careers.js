b6582e0a16ead20ed9a1cea43b2eef42045a52eb
Alexander <jdaanov@gmail.com>
2023-11-13 23:36:10 -0800

    Update test_careers.js

--- tests/test_careers.js
+++ tests/test_careers.js
@@ -4,32 +4,31 @@ const CommonPage = require('../pageObjects');
 const { CAREERS_BUTTON_HEADER } = require('../locators');
 
 class CareersPage extends CommonPage {
-  async navigateToCareers() {
-    await this.page.click(CAREERS_BUTTON_HEADER); // careers selector
-  }
+    async navigateToCareers() {
+        await this.page.click(CAREERS_BUTTON_HEADER); // Careers selector
+    }
 }
 
 (async () => {
-  let browser, page;
+    let browser, page;
 
-  try {
-    ({ browser, page } = await setupBrowser());
-    const commonPage = new CommonPage(page);
-    const careersPage = new CareersPage(page);
+    try {
+        ({ browser, page } = await setupBrowser());
+        const commonPage = new CommonPage(page);
+        const careersPage = new CareersPage(page);
 
-  try {
-    // Test 2: Validate that clicking the "Careers" button navigates to "https://www.rdbrck.com/careers".
-    await commonPage.navigateTo('https://www.rdbrck.com');
-    await careersPage.navigateToCareers();
-    const currentUrl = page.url();
-    if (currentUrl === 'https://www.rdbrck.com/careers') {
-      console.log('Test 2 Passed: Navigation to Careers successful');
-    } else {
-      console.error(`Test 2 Failed: Expected navigation to https://www.rdbrck.com/careers, but got ${currentUrl}`);
+        // Test 2: Validate that clicking the "Careers" button navigates to "https://www.rdbrck.com/careers".
+        await commonPage.navigateTo('https://www.rdbrck.com');
+        await careersPage.navigateToCareers();
+        const currentUrl = page.url();
+        if (currentUrl === 'https://www.rdbrck.com/careers') {
+            console.log('Test 2 Passed: Navigation to Careers successful');
+        } else {
+            console.error(`Test 2 Failed: Expected navigation to https://www.rdbrck.com/careers, but got ${currentUrl}`);
+        }
+    } catch (error) {
+        console.error('Error during test:', error);
+    } finally {
+        await browser.close();
     }
-  } catch (error) {
-    console.error('Error during test:', error);
-  } finally {
-    await browser.close();
-  }
 })();
