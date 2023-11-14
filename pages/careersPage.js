d70917c3a98c3def0bc45994a34ef11d7ae6f240
Alexander <jdaanov@gmail.com>
2023-11-14 09:32:48 -0800

    Update careersPage.js

--- pages/careersPage.js
+++ pages/careersPage.js
@@ -1,1 +1,10 @@
+const BasePage = require('./basePage');
+const { CAREERSPAGE_LOCATORS } = require('../locators/careersPageLocators');
 
+class CareersPage extends BasePage {
+  
+  // careersPage-specific methods will add here
+  
+}
+
+module.exports = CareersPage;
