b8aefb264410c2eed091c4602b4ae6a9ae68b481
Alexander <jdaanov@gmail.com>
2023-11-14 15:13:53 -0800

    Update careersPageLocators.js

--- locators/careersPageLocators.js
+++ locators/careersPageLocators.js
@@ -1,11 +1,7 @@
+// careersPageLocators.js
+
 // Careers site menu button
-const CAREERS_BUTTON_HEADER = 'li a[href*="careers"]';
+exports.CAREERS_BUTTON_HEADER = 'li a[href*="careers"]';
 
 // Open position titles
-const OPEN_POSITION_TITLES = '.pinion.pinion-openPositions h5';
-
-module.exports = {
-  CAREERS_BUTTON_HEADER,
-  OPEN_POSITION_TITLES,
-  // Will add more locators as needed
-};
+exports.OPEN_POSITION_TITLES = '.pinion.pinion-openPositions h5';
