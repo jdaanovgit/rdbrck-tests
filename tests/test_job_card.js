520dba2c4549d62eec2b7f9a2e73bff09f35bca5
Alexander <jdaanov@gmail.com>
2023-11-14 15:03:11 -0800

    Update test_job_card.js

--- tests/test_job_card.js
+++ tests/test_job_card.js
@@ -1,29 +1,31 @@
 const assert = require('assert');
-const getOpenPositionTitles = require('../getOpenPositionTitles');
+const getOpenPositionTitles = require('../getOpenOpenPositionTitles');
 const { setupBrowser } = require('../setupBrowser');
 
 (async () => {
   const { browser, page } = await setupBrowser();
 
   try {
-    // Navigate directly using the page object
+    console.log('Navigating to the Careers page...');
     await page.goto('https://www.rdbrck.com/careers');
+    console.log('Navigation successful.');
 
-    // Use the getOpenPositionTitles method to get a list of open positions
+    console.log('Fetching open position titles...');
     const openPositionTitles = await getOpenPositionTitles(page);
+    console.log('Open position titles:', openPositionTitles);
 
     // Validation: Check if "Lead QA Automation Developer" is in the list of open positions
     const positionToFind = 'Lead QA Automation Developer';
     assert(openPositionTitles.includes(positionToFind), `${positionToFind} - does not appear in the list`);
-
     console.log(`${positionToFind} - found in the list`);
 
     // If the position is found = test passed
     console.log('Test Passed!');
-
   } catch (error) {
-    console.error('Error:', error.message);
+    console.error('Error during test:', error.message);
   } finally {
+    console.log('Closing the browser...');
     await browser.close();
+    console.log('Browser closed.');
   }
 })();
