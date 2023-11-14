76ac4d74442e8ad2b6896897f9dc9b0a78c48b02
Alexander <jdaanov@gmail.com>
2023-11-14 13:31:07 -0800

    Update test_navigate_poc.js

--- tests/test_navigate_poc.js
+++ tests/test_navigate_poc.js
@@ -1,35 +1,29 @@
-
-const BaseTest = require('./baseTest');
+const BaseTest = require('./baseTest'); 
 const CommonPage = require('../pageObjects');
 
-class TestNavigate extends BaseTest {
-  async runTest() {
-    let browser, page;
+const runTest = async () => {
+  try {
+    // Setup browser and page
+    const { browser, page } = await BaseTest.setup();
 
-    try {
-      // Setup browser and page
-      ({ browser, page } = await this.setup());
+    // Test logic
+    const commonPage = new CommonPage(page);
+    await commonPage.navigateTo('https://rdbrck.com');
+    const currentUrl = page.url();
 
-      // Test logic
-      const commonPage = new CommonPage(page);
-      await commonPage.navigateTo('https://rdbrck.com');
-      const currentUrl = page.url();
-
-      // Validation section
-      if (currentUrl === 'https://www.rdbrck.com/') {
-        console.log('Test Passed: Redirect successful');
-      } else {
-        console.error(`Test Failed: Expected redirection to https://www.rdbrck.com, but got ${currentUrl}`);
-      }
-    } catch (error) {
-      console.error('Error during test:', error);
-    } finally {
-      // Close browser
-      await this.close();
+    // Validation
+    if (currentUrl === 'https://www.rdbrck.com/') {
+      console.log('Test Passed: Redirect successful');
+    } else {
+      console.error(`Test Failed: Expected redirection to https://www.rdbrck.com, but got ${currentUrl}`);
     }
+  } catch (error) {
+    console.error('Error during test:', error);
+  } finally {
+    // Close browser
+    await BaseTest.close({ browser });
   }
-}
+};
 
 // Run the test
-const testNavigate = new TestNavigate();
-testNavigate.runTest();
+runTest();
