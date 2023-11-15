debe16b3b8b0b525c7fe570145670de66fbfdd3c
Alexander <jdaanov@gmail.com>
2023-11-14 19:34:55 -0800

    Update getOpenPositionTitles.js

--- getOpenPositionTitles.js
+++ getOpenPositionTitles.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const careersLocator = require('./locators/careersPageLocators');
+const careersLocator = require('./locators');
 
 console.log('careersLocator:', careersLocator);
 
