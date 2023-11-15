3a0e1bea7a7008fe5fb60fb3e9409e839e5d9ff2
Alexander <jdaanov@gmail.com>
2023-11-14 19:34:29 -0800

    Update locators.js

--- locators.js
+++ locators.js
@@ -1,7 +1,10 @@
 // Careers button
 const CAREERS_BUTTON_HEADER = 'li a[href*="careers"]';
 
+const OPEN_POSITION_TITLES = '.pinion.pinion-openPositions h5';
+
 module.exports = {
   CAREERS_BUTTON_HEADER,
+  OPEN_POSITION_TITLES,
   // will add more locators as needed
 };
